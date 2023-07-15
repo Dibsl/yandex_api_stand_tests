3b3cd1f40d3be8054bfe739aa426c1e12b4e70b0
DIBSL_mac <dibsl_mac@MacBook-Pro-1234.local>
2023-07-15 20:56:19 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://043c07f7-5d1e-45bb-a85e-87183cd06b1b.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
